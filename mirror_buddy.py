391fa115b67b7b64a38baed2e77273b09419c95f
Tyler Lang <tyler@localhost.localdomain>
2021-12-05 11:51:18 -0800

    fixed the ping_mirror function

--- mirror_buddy.py
+++ mirror_buddy.py
@@ -5,11 +5,12 @@ import httplib2
 from bs4 import BeautifulSoup, SoupStrainer
 import os
 
-#pings the mirrors
-def ping_mirror(url):
-    for url in mirrors:
-        os.popen(url)
+#pings the mirrors          https://mirrors.opensuse.org
 
+def ping_mirror(hostname):
+    response = os.system('ping -c 1 ' + hostname)
+
+ 
 #pulls the list of mirrors from mirrors.opensuse.org
 def mirror_list(url, http):   
     response, content = http.request(url)
@@ -21,8 +22,8 @@ def mirror_list(url, http):
     mirrors.remove("https://bugzilla.opensuse.org/")
     mirrors.remove("https://github.com/openSUSE/mirrorbrain")
 
-    print(mirrors)
+    return mirrors
 
-mirror_list('https://mirrors.opensuse.org', httplib2.Http())
+
 
 
